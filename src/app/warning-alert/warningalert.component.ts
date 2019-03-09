5cc6e737abb844dd407f8f942de7e98e191ae417
mahaDev <lakshman.kotipalli@outlook.com>
2019-03-09 23:04:06 +0530

    Adding success and warning components

---
+++ src/app/warning-alert/warningalert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warningalert.component.html',
+  styleUrls: ['warningalert.component.scss']
+})
+
+export class WarningAlertComponent {
+
+}
